68aab1c2d0e9d36633e83432dccd4ee8086b9213
jyjang <jinyngg@gmail.com>
2023-05-22 21:24:45 +0900

    test  : 에러에 대한 공통 응답 결과 테스트 (#25)

--- src/test/java/com/zerobase/account/controller/AccountControllerTest.java
+++ src/test/java/com/zerobase/account/controller/AccountControllerTest.java
@@ -5,9 +5,11 @@ import com.zerobase.account.domain.Account;
 import com.zerobase.account.dto.AccountDto;
 import com.zerobase.account.dto.CreateAccount;
 import com.zerobase.account.dto.DeleteAccount;
+import com.zerobase.account.exception.AccountException;
 import com.zerobase.account.service.AccountService;
 import com.zerobase.account.service.RedisTestService;
 import com.zerobase.account.type.AccountStatus;
+import com.zerobase.account.type.ErrorCode;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -139,6 +141,23 @@ class AccountControllerTest {
                 .andExpect(jsonPath("$[2].accountNumber").value("3456789012"))
                 .andExpect(jsonPath("$[2].balance").value(3000L))
                 .andDo(print());
+
+    }
+
+    @Test
+    void failedGetAccount() throws Exception {
+
+        //given
+        given(accountService.getAccount(anyLong()))
+                .willThrow(new AccountException(ErrorCode.ACCOUNT_NOT_FOUND));
+
+        //when
+        //then
+        mockMvc.perform(get("/account/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.errorCode").value("ACCOUNT_NOT_FOUND"))
+                .andExpect(jsonPath("$.errorMessage").value("계좌가 없습니다."))
+                .andDo(print()) ;
 
     }
 
