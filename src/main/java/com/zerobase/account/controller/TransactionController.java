6683e0d83a6ac5a01ba93c7e58d6aab234494fc3
jyjang <jinyngg@gmail.com>
2023-05-22 21:14:26 +0900

    feat  : 잔액 사용 확인 메소드 추가 (#21)

--- src/main/java/com/zerobase/account/controller/TransactionController.java
+++ src/main/java/com/zerobase/account/controller/TransactionController.java
@@ -1,14 +1,13 @@
 package com.zerobase.account.controller;
 
 import com.zerobase.account.dto.CancelBalance;
+import com.zerobase.account.dto.QueryTransactionResponse;
 import com.zerobase.account.dto.UseBalance;
 import com.zerobase.account.exception.AccountException;
 import com.zerobase.account.service.TransactionService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -62,13 +61,18 @@ public class TransactionController {
 
             transactionService.saveFailedCancelTransaction(
                     request.getAccountNumber()
-                    ,request.getAmount()
+                    , request.getAmount()
             );
 
             throw e;
         }
     }
 
-
-
+    @GetMapping("/transaction/{transactionId}")
+    public QueryTransactionResponse queryTransaction(
+            @PathVariable String transactionId
+    ) {
+        return QueryTransactionResponse.from(
+                transactionService.queryTransaction(transactionId));
+    }
 }
